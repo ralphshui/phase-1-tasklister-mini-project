fa6662f0465b18fe1deb726f368a78ea620f7bc2
Ralph Shui <rskc.2112@gmail.com>
2023-06-21 09:18:47 -0400

    completing tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+let someNum = 1;
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const inputBox = document.getElementById('new-task-description');
+  
+  const btn = document.getElementById('create-task-form');
+  btn.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(inputBox.value === ''){
+      return;
+    }
+
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const ul = document.createElement('ul');
+    
+    div.id = 'list';
+    div.className = `card-${someNum++}`;
+    ul.id = 'tasks';
+
+    h2.textContent = 'To Do';
+
+    const toDoLi = document.createElement('li');
+    toDoLi.textContent = inputBox.value;
+    
+    inputBox.value = '';
+
+    div.addEventListener('click', () => div.remove())
+    
+    div.append(h2, ul);
+    ul.append(toDoLi);
+    document.body.append(div);
+  });
 });
